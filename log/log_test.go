89cceee1e0d56ce2d55805e4ca1947a43121ed11
feiyudev <dev@feiyudev.local>
2019-08-05 11:50:55 +0800

    util log

---
+++ log/log_test.go
@@ -0,0 +1,18 @@
+package log_test
+
+import (
+	dlog "github.com/tagDong/dutil/log"
+	"testing"
+)
+
+func TestNewLogger(t *testing.T) {
+	logger := dlog.NewLogger("log", "testLog")
+
+	logger.Infoln("infoln message", 1)
+	logger.Infof("%s : %d", "infof message", 2)
+	logger.Debugln("Debugln message", 1)
+	logger.Debugf("%s : %d", "Debugf message", 2)
+	logger.Errorln("Errorln message", 1)
+	logger.Errorf("%s : %d", "Errorf message", 2)
+
+}
