9c471722e04543add56393b0059c56f221371b3c
dong <248244142@qq.com>
2019-08-30 14:27:48 +0800

    log_test

--- log/log_test.go
+++ log/log_test.go
@@ -3,18 +3,21 @@ package log_test
 import (
 	dlog "github.com/tagDong/dutil/log"
 	"testing"
+	"time"
 )
 
 func TestNewLogger(t *testing.T) {
-	logger := dlog.NewLogger("log", "testLog")
+	logger := dlog.NewLogger("log", "testLog", 100)
 	//dlog.CloseStdOut()
 	//logger.AsyncOut()
 	logger.SetOutLevel(dlog.DEBUG, dlog.INFO)
 
 	logger.Infoln("infoln message", 1)
 	logger.Infof("%s : %d", "infof message", 2)
+	time.Sleep(time.Second)
 	logger.Debugln("Debugln message", 1)
 	logger.Debugf("%s : %d", "Debugf message", 2)
+	time.Sleep(time.Second)
 	logger.Errorln("Errorln message", 1)
 	logger.Errorf("%s : %d", "Errorf message", 2)
 
