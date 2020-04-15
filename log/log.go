6330ca289fbeab8e07de49a32c5ecc98c13814bc
yidongdeng <248244142@qq.com>
2020-04-15 10:51:59 +0800

    none

--- log/log.go
+++ log/log.go
@@ -8,19 +8,6 @@ import (
 	"time"
 )
 
-type LoggerI interface {
-	Debugln(v ...interface{})
-	Debugf(format string, v ...interface{})
-	Infoln(v ...interface{})
-	Infof(format string, v ...interface{})
-	Warnln(v ...interface{})
-	Warnf(format string, v ...interface{})
-	Errorln(v ...interface{})
-	Errorf(format string, v ...interface{})
-	Fataln(v ...interface{})
-	Fatalf(format string, v ...interface{})
-}
-
 type Level int
 
 const (
