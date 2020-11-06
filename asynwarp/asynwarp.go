5dce1667f0d7fe338bea6d5843c81bb93acba1b0
yidongdeng <248244142@qq.com>
2020-11-06 10:55:30 +0800

    wrap

--- asynwarp/asynwarp.go
+++ asynwarp/asynwarp.go
@@ -10,20 +10,29 @@ func WrapFunc(oriFunc interface{}) func(callback func([]interface{}), args ...in
 	if oriF.Kind() != reflect.Func {
 		return nil
 	}
+	fnType := reflect.TypeOf(oriF)
 
 	return func(callback func([]interface{}), args ...interface{}) {
 		f := func() {
-			in := []reflect.Value{}
-			for _, v := range args {
-				in = append(in, reflect.ValueOf(v))
+			var in []reflect.Value
+			numIn := fnType.NumIn()
+			if numIn > 0 {
+				in = make([]reflect.Value, numIn)
+				for i := 0; i < numIn; i++ {
+					if i >= len(args) || args[i] == nil {
+						in[i] = reflect.Zero(fnType.In(i))
+					} else {
+						in[i] = reflect.ValueOf(args[i])
+					}
+				}
 			}
+
 			out := oriF.Call(in)
 
 			if len(out) > 0 {
-
-				ret := make([]interface{}, 0, len(out))
-				for _, v := range out {
-					ret = append(ret, v.Interface())
+				ret := make([]interface{}, len(out))
+				for i, v := range out {
+					ret[i] = v.Interface()
 				}
 				if nil != callback {
 					callback(ret)
@@ -34,6 +43,7 @@ func WrapFunc(oriFunc interface{}) func(callback func([]interface{}), args ...in
 				}
 			}
 		}
+
 		go f()
 	}
 }
